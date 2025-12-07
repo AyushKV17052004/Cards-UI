a529d88c3d66de553a79136a2cb0dee1f3b239d3
Ayush Verma <akv019.btech2024@cse.nitrr.ac.in>
2025-12-07 12:10:34 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,83 @@
+import { useState } from 'react'
+import data from './Data'
+import './App.css'
+import Courses from './Componenets/Courses'
+import { ToastContainer } from 'react-toastify';
+import "react-toastify/ReactToastify.css";
+
+
+import Typewriter from "typewriter-effect";
+
+function App() {
+
+const [active , setActive] = useState(0);    
+const [DATA , setData] = useState(data);
+
+
+function AllHandler(){
+   const newData = data;
+setData(newData); 
+setActive(0);
+ 
+}
+
+function devHandler(){
+const newData = data.filter(d=>d.type === "Development");
+setData(newData);
+setActive(1);
+ 
+}
+function businessHandler(){
+const newData = data.filter(d=>d.type === "Business");
+setData(newData);
+setActive(2);
+ 
+}
+function designHandler(){
+const newData = data.filter(d=>d.type === "Design");
+setData(newData);
+setActive(3);
+ 
+}
+function lifestyleHandler(){
+const newData = data.filter(d=>d.type === "Lifestyle");
+setData(newData);
+ 
+}
+
+
+ return(
+    <div className='bg-gray-500 h-full min-h-screen' >
+      <div className='w-full mx-auto bg-gray-900 py-5 text-center text-4xl text-white font-bold'>
+       <Typewriter
+          options={{
+            strings: [
+              "Web Developer ?",
+              "UI/UX Designer ?",
+              "Lifestyle ?",
+              "Just Choose the Course"
+            ],
+            autoStart: true,
+            loop: true,
+            delay: 60,
+            deleteSpeed: 100,
+          }}
+        />
+        </div>
+        <div className='pb-3'>
+            <div className=' w-1/3 max-[700px]:w-3/4 max-[700px]:gap-y-2 flex flex-row flex-wrap justify-evenly items-center mx-auto space-x-3 my-5'>
+                <button onClick={AllHandler} className={`bg-gray-700 px-2 py-1 text-white text-lg font-bold rounded-md hover:bg-gray-950 cursor-pointer ${active===0?"border-2 border-white bg-gray-950" : "border-0 bg-gray-700"} `}>All</button>
+                <button onClick={devHandler} className={`bg-gray-700 px-2 py-1 text-white text-lg font-bold rounded-md hover:bg-gray-950 cursor-pointer ${active===1?"border-2 border-white bg-gray-950" : "border-0 bg-gray-700"} `}>Development</button>
+                <button onClick={businessHandler} className={`bg-gray-700 px-2 py-1 text-white text-lg font-bold rounded-md hover:bg-gray-950 cursor-pointer ${active===2?"border-2 border-white bg-gray-950" : "border-0 bg-gray-700"} `}>Business</button>
+                <button onClick={designHandler} className={`bg-gray-700 px-2 py-1 text-white text-lg font-bold rounded-md hover:bg-gray-950 cursor-pointer ${active===3?"border-2 border-white bg-gray-950" : "border-0 bg-gray-700"} `}>Design</button>
+                <button onClick={lifestyleHandler} className={`bg-gray-700 px-2 py-1 text-white text-lg font-bold rounded-md hover:bg-gray-950 cursor-pointer ${active===4?"border-2 border-white bg-gray-950" : "border-0 bg-gray-700"} `}>LifeStyle</button>
+            </div>
+        (<Courses data = {DATA} ></Courses>) 
+        </div>
+        <ToastContainer></ToastContainer>
+        </div>
+ )
+     
+}
+
+export default App
---
+++ src/Data.jsx
@@ -0,0 +1,151 @@
+const data = [
+    {
+      id: 1,
+      title: "Web Development Fundamentals" ,
+      imgSrc: "https://codehelp-apis.vercel.app/get-top-courses/Development/Web%20Development%20Fundamentals.png" ,
+      info:"This course covers the basic concepts and tools for building static and dynamic websites. Students w..." ,
+      type: "Development"  ,
+      icon: "src\assets\heart.png"
+
+    },
+       {
+      id: 2,
+      title: "Data Science Essentials" ,
+      imgSrc: "https://codehelp-apis.vercel.app/get-top-courses/Development/Data%20Science%20Essentials.png" ,
+      info:"This course provides an introduction to the fundamental concepts and techniques used in data science.." ,
+      type: "Development"  ,
+      icon: "src\assets\heart.png"
+
+    },
+       {
+      id: 3,
+      title: "DevOps for Agile Teams" ,
+      imgSrc: "https://codehelp-apis.vercel.app/get-top-courses/Development/Devops%20for%20Agile%20Teams.png" ,
+      info:"This course provides an overview of DevOps principles and practices, with a focus on how they can be..." ,
+      type: "Development"  ,
+      icon: "src\assets\heart.png"
+
+    },
+       {
+      id: 4,
+      title: "Software Testing Fundamentals" ,
+      imgSrc: "https://codehelp-apis.vercel.app/get-top-courses/Development/Software%20Testing%20Fundamentals.png" ,
+      info:"This course covers the basic concepts and tools for building static and dynamic websites. Students w..." ,
+      type: "Development"  ,
+      icon: "src\assets\heart.png"
+
+    },
+       {
+      id: 5,
+      title: "Mobile App Development with Native" ,
+      imgSrc: "https://codehelp-apis.vercel.app/get-top-courses/Development/Mobile%20App%20Development%20With%20React%20Native.png" ,
+      info:"This course teaches students how to build native mobile apps using the React Native framework. Stude..." ,
+      type: "Development"  ,
+      icon: "src\assets\heart.png"
+
+    },
+        {
+      id: 6,
+      title: "Introduction to Marketing" ,
+      imgSrc: "https://codehelp-apis.vercel.app/get-top-courses/Business/Introduction%20To%20Marketing.png" ,
+      info:"This course provides an overview of marketing principles and practices, including market research, s..." ,
+      type: "Business"  ,
+      icon: "src\assets\heart.png"
+
+    },
+         {
+      id: 7,
+      title: "Leadership Development" ,
+      imgSrc: "https://codehelp-apis.vercel.app/get-top-courses/Business/Leadership%20Development.png" ,
+      info:"This course covers the fundamental concepts and skills required for effective leadership, including ..." ,
+      type: "Business"  ,
+      icon: "src\assets\heart.png"
+
+    },
+         {
+      id: 8,
+      title: "Project Management Essentials" ,
+      imgSrc: "https://codehelp-apis.vercel.app/get-top-courses/Business/Project%20Management%20Essentials.png" ,
+      info:"This course provides an overview of project management principles and techniques, including project ..." ,
+      type: "Business"  ,
+      icon: "src\assets\heart.png"
+
+    },
+         {
+      id: 9,
+      title: "Finance for Non-Financial Managers" ,
+      imgSrc: "https://codehelp-apis.vercel.app/get-top-courses/Business/Finance%20for%20Non-Financial%20Managers.png" ,
+      info:"This course provides an introduction to finance concepts and tools for non-financial managers. Stude..." ,
+      type: "Business"  ,
+      icon: "src\assets\heart.png"
+
+    },
+         {
+      id: 10,
+      title: "Operations Management Fundamentals" ,
+      imgSrc: "https://codehelp-apis.vercel.app/get-top-courses/Business/Operations%20Management%20Fundamentals.png" ,
+      info:"This course covers the basics of operations management, including process design, capacity planning,..." ,
+      type: "Business"  ,
+      icon: "src\assets\heart.png"
+
+    },
+
+       {
+      id: 11,
+      title: "User Experience Design" ,
+      imgSrc: "https://codehelp-apis.vercel.app/get-top-courses/Design/User%20Experience%20Design.png" ,
+      info:"This course covers the fundamentals of user experience (UX) design, including user research, interac..." ,
+      type: "Design"  ,
+      icon: "src\assets\heart.png"
+
+    },
+       {
+      id: 12,
+      title: "Graphic Design Fundamentals" ,
+      imgSrc: "https://codehelp-apis.vercel.app/get-top-courses/Design/Graphic%20Design%20Fundamentals.png" ,
+      info:"This course covers the fundamental concepts and techniques used in industrial design, including sket..." ,
+      type: "Design"  ,
+      icon: "src\assets\heart.png"
+
+    },
+       {
+      id: 13,
+      title: "Fashion Design Principles" ,
+      imgSrc: "https://codehelp-apis.vercel.app/get-top-courses/Design/Fashion%20Design%20Principles.png" ,
+      info:"This course covers the principles and practices of fashion design, including fashion illustration, t..." ,
+      type: "Design"  ,
+      icon: "src\assets\heart.png"
+
+    },
+       {
+      id: 14,
+      title: "Cooking Basics" ,
+      imgSrc: "https://codehelp-apis.vercel.app/get-top-courses/Lifestyle/Cooking%20Basics.png" ,
+      info:"This course provides an introduction to cooking techniques, ingredients, and flavors. Students will ..." ,
+      type: "Lifestyle"  ,
+      icon: "src\assets\heart.png"
+
+    },
+         {
+      id: 15,
+      title: "Fitness for Life" ,
+      imgSrc: "https://codehelp-apis.vercel.app/get-top-courses/Lifestyle/Fitness%20For%20Life.png" ,
+      info:"This course covers the fundamentals of fitness, including exercise principles, workout planning, and..." ,
+      type: "Lifestyle"  ,
+      icon: "src\assets\heart.png"
+
+    },
+         {
+      id: 16,
+      title: "Travel Photography" ,
+      imgSrc: "https://codehelp-apis.vercel.app/get-top-courses/Lifestyle/Travel%20Photography.png" ,
+      info:"This course covers the fundamentals of travel photography, including composition, lighting, and stor..." ,
+      type: "Lifestyle"  ,
+      icon: "src\assets\heart.png"
+
+    },
+   
+
+]
+
+export default data;
